abc1b050d56b589471c0b3f5d03353fdb3f57657
Wellington Oliveira <well.oliveira.snt@gmail.com>
2020-06-06 20:14:44 -0400

    Missing Memo on react-is at build
     - Update libs
     - Fix margins

--- src/components/Login/storyHelper.js
+++ src/components/Login/storyHelper.js
@@ -6,16 +6,16 @@ import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
+const style = { width: '100%', backgroundColor: '#fff' };
 export const InputPassword = () => {
   const [showPassword, setShowPassword] = useState(false);
-
-  const style = { width: '100%', backgroundColor: '#fff' };
   return (
     <Input
       name="login_senha"
       label="Senha"
       style={style}
       type={showPassword ? 'text' : 'password'}
+      margin="none"
       InputProps={{
         startAdornment: (
           <InputAdornment position="start">
